e4f7065dc5308fcae6f66dbe2a5d7206aa963037
JuveyGaymer XD <juvey746@gmail.com>
2024-05-13 18:42:32 -0700

    Se editoe el gymviewmodel y gymview

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,5 +70,6 @@ dependencies {
     implementation("androidx.compose.runtime:runtime-livedata:1.6.7")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("io.coil-kt:coil-compose:2.6.0")
 
 }
--- app/src/main/java/com/juvey/examen/screens/gyms/network/GymService.kt
+++ app/src/main/java/com/juvey/examen/screens/gyms/network/GymService.kt
@@ -4,6 +4,9 @@ import com.juvey.examen.screens.gyms.models.Gym
 import retrofit2.http.GET
 
 interface GymService {
-    @GET("JuveyVa/23f53cd7cf4b0791c3ab8e5a159d75f3/raw/db29bf2737bcdd7fb012b8be5914b617b14c7ba1/")
+    // GET se puede ver en el navegador
+    // POST o revisar informacion
+    // PUT actualizar datos
+    @GET("JuveyVa/23f53cd7cf4b0791c3ab8e5a159d75f3/raw/1fa40a43a158496d13889a229fbb8710cf5ca0ce/gyms.json")
     suspend fun getGyms(): List<Gym>
 }
